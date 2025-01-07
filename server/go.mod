8e3bb6c3cbc8f52d3b59ec6463a5791b1e7cb372
Justyn Temme <justyntemme@gmail.com>
2025-01-06 19:52:21 -0600

    change structure in order not to keep response writers in a connection pool

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/justyntemme/gobolt/server
+
+go 1.23.4
---
+++ server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Start initializes and starts the HTTP server.
+func Start(port string) error {
+	// Register routes
+	// registerRoutes()
+
+	// Start the HTTP server
+	fmt.Printf("Server listening on http://localhost%s\n", port)
+	return http.ListenAndServe(port, nil)
+}
